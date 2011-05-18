11bf53e094ee535ea41c6197d903cbcab6037213
Zsolt Bitvai <bitvaizs@gmail.com>
2011-05-18 22:33:58 +0100

    Added openUrl and isXml methods

--- src/uk/ac/shef/semweb/UrlFileReader.java
+++ src/uk/ac/shef/semweb/UrlFileReader.java
@@ -2,14 +2,26 @@ package uk.ac.shef.semweb;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
 public class UrlFileReader {
 
     public static final String INPUT_PATH = "input/internalLinks.txt";
+    public static final String XML_TYPE = "text/xml";
 
+    /**
+     * Reads a file and returns each line as a String
+     */
     public List<String> readFile(String inputPath) throws FileNotFoundException {
 	File inputFile = new File(inputPath);
 	Scanner scanner = new Scanner(inputFile);
@@ -19,5 +31,23 @@ public class UrlFileReader {
 	    urls.add(scanner.nextLine());
 	}
 	return urls;
+    }
+
+    /**
+     * Opens the url
+     */
+    public HttpEntity openUrl(String url) throws ClientProtocolException, IOException {
+	HttpClient httpclient = new DefaultHttpClient();
+	HttpGet httpget = new HttpGet(url);
+	HttpResponse response = httpclient.execute(httpget);
+	HttpEntity entity = response.getEntity();
+	return entity;
+    }
+
+    /**
+     * Is the entity an xml file?
+     */
+    public boolean isXML(HttpEntity entity) {
+	return entity.getContentType().getValue().equals(XML_TYPE);
     }
 }
--- test/uk/ac/shef/semweb/UrlFileReaderTest.java
+++ test/uk/ac/shef/semweb/UrlFileReaderTest.java
@@ -1,10 +1,17 @@
 package uk.ac.shef.semweb;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.List;
 
 import junit.framework.TestCase;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.junit.Before;
 
 
@@ -21,6 +28,19 @@ public class UrlFileReaderTest extends TestCase {
 	List<String> urls = this.urlFileReader.readFile(UrlFileReader.INPUT_PATH);
 	assertFalse(urls.isEmpty());
 	assertFalse(urls.get(0).isEmpty());
+	
+    }
+    public void testOpenUrl() throws ClientProtocolException, IOException{
+	String url = "http://poplar.dcs.shef.ac.uk/~u0082/intelweb2/%3fq=taxonomy/term/1&amp;page=7";
+	HttpEntity entity = this.urlFileReader.openUrl(url);
+	assertNotNull(entity);
+    }
+    public void testIsXML() throws ClientProtocolException, IOException{
+	String notXmlUrl = "http://poplar.dcs.shef.ac.uk/~u0082/intelweb2/%3fq=taxonomy/term/1&amp;page=7";
+	assertFalse(this.urlFileReader.isXML(this.urlFileReader.openUrl(notXmlUrl)));
+	
+	String isXmlUrl = "http://poplar.dcs.shef.ac.uk/~u0082/intelweb2/%3fq=users/user10/xml";
+	assertTrue(this.urlFileReader.isXML(this.urlFileReader.openUrl(isXmlUrl)));
 	
     }
 }
