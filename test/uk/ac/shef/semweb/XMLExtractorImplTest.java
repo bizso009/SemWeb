29167f2129856448641c4264fb10e1c02ca2c409
Zsolt Bitvai <bitvaizs@gmail.com>
2011-05-19 07:57:26 +0100

    refactored some more

--- test/uk/ac/shef/semweb/XMLExtractorImplTest.java
+++ test/uk/ac/shef/semweb/XMLExtractorImplTest.java
@@ -1,11 +1,16 @@
 package uk.ac.shef.semweb;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.List;
 
 import junit.framework.TestCase;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.client.ClientProtocolException;
 import org.junit.Before;
+
+import com.hp.hpl.jena.rdf.model.Model;
 
 
 public class XMLExtractorImplTest extends TestCase {
@@ -18,9 +23,30 @@ public class XMLExtractorImplTest extends TestCase {
 	this.extractor = new XMLExtractorImpl();
     }
     
-    public void testGetAllUrls() throws FileNotFoundException{
-	List<String> urls = this.extractor.getAllUrls();
+    public void testReadFile() throws FileNotFoundException{
+	List<String> urls = this.extractor.readFile(XMLExtractorImpl.INPUT_PATH);
 	assertFalse(urls.isEmpty());
+	assertFalse(urls.get(0).isEmpty());
+	
+    }
+    public void testOpenUrl() throws ClientProtocolException, IOException{
+	String url = "http://poplar.dcs.shef.ac.uk/~u0082/intelweb2/%3fq=taxonomy/term/1&amp;page=7";
+	HttpEntity entity = this.extractor.openUrl(url);
+	assertNotNull(entity);
+    }
+    public void testIsXML() throws ClientProtocolException, IOException{
+	String notXmlUrl = "http://poplar.dcs.shef.ac.uk/~u0082/intelweb2/%3fq=taxonomy/term/1&amp;page=7";
+	assertFalse(this.extractor.isXML(this.extractor.openUrl(notXmlUrl)));
+	
+	String isXmlUrl = "http://poplar.dcs.shef.ac.uk/~u0082/intelweb2/%3fq=users/user10/xml";
+	assertTrue(this.extractor.isXML(this.extractor.openUrl(isXmlUrl)));
+	
+    }
+    public void testParseRdf() throws IllegalStateException, ClientProtocolException, IOException{
+	Model model = this.extractor.parseRdf(XMLExtractorImpl.ONTOLOGY_URL);
+	model.write(System.out, "N-TRIPLE");
+	assertNotNull(model);
+	
     }
     
 }
