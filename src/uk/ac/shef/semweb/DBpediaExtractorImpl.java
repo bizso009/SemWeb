c8882de42e2b2f26be5ad87d7ebe20a82fef880a
Kushal D'Souza <kushlyn@gmail.com>
2011-05-19 07:55:37 +0100

    Name change

--- src/uk/ac/shef/semweb/DBpediaExtractorImpl.java
+++ src/uk/ac/shef/semweb/DBpediaExtractorImpl.java
@@ -1,11 +1,7 @@
 package uk.ac.shef.semweb;
 
-public class DBPediaFileReader extends XMLExtractorImpl implements DBpediaExtractor
+public class DBpediaExtractorImpl extends XMLExtractorImpl implements DBpediaExtractor
 {
-	public DBPediaFileReader()
-	{
-	}
-
 	@Override
 	public void getDBpediaInfo(String URL) {
 		// TODO Auto-generated method stub
