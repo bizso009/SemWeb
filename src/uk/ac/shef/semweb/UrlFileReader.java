62030e2eb82f5cdc717a8703f0b520c6c67c1305
Zsolt Bitvai <bitvaizs@gmail.com>
2011-05-19 05:56:51 +0100

    conflicts

--- src/uk/ac/shef/semweb/UrlFileReader.java
+++ src/uk/ac/shef/semweb/UrlFileReader.java
@@ -20,7 +20,6 @@ import com.hp.hpl.jena.rdf.model.ModelFactory;
 
 public class UrlFileReader {
 
-<<<<<<< HEAD
     public static final String INPUT_PATH = "input/internalLinks.txt";
     public static final String XML_TYPE = "text/xml";
     public static final String ONTOLOGY_URL = "http://poplar.dcs.shef.ac.uk/~u0082/intelweb2/intelweb.rdf";
@@ -39,36 +38,6 @@ public class UrlFileReader {
 	return urls;
     }
 
-    /**
-     * Opens the url
-     */
-    public HttpEntity openUrl(String url) throws ClientProtocolException,
-	    IOException {
-	HttpClient httpclient = new DefaultHttpClient();
-	HttpGet httpget = new HttpGet(url);
-	HttpResponse response = httpclient.execute(httpget);
-	HttpEntity entity = response.getEntity();
-	return entity;
-    }
-=======
-	public static final String INPUT_PATH = "input/internalLinks.txt";
-	public static final String XML_TYPE = "text/xml";
-	public static final String ONTOLOGY_URL = "http://poplar.dcs.shef.ac.uk/~u0082/intelweb2/intelweb.rdf";
-
-	/**
-	 * Reads a file and returns each line as a String
-	 */
-	public List<String> readFile(String inputPath) throws FileNotFoundException {
-		File inputFile = new File(inputPath);
-		Scanner scanner = new Scanner(inputFile);
-
-		List<String> urls = new ArrayList<String>();
-		while (scanner.hasNext()) {
-			urls.add(scanner.nextLine());
-		}
-		return urls;
-	}
->>>>>>> 7db5d37fbf6feb47aa71ce6db4f53597d1ca36db
 
 	/**
 	 * Opens the url
@@ -82,23 +51,12 @@ public class UrlFileReader {
 		return entity;
 	}
 
-<<<<<<< HEAD
-    public Model parseRdf(String ontologyUrl) throws IllegalStateException,
-	    ClientProtocolException, IOException {
-	Model model = ModelFactory.createDefaultModel();
 
-	// use the FileManager to find the input file
-	InputStream in = openUrl(ontologyUrl).getContent();
-	if (in == null) {
-	    throw new IllegalArgumentException("File: " + ontologyUrl
-		    + " not found");
-=======
 	/**
 	 * Is the entity an xml file?
 	 */
 	public boolean isXML(HttpEntity entity) {
 		return entity.getContentType().getValue().equals(XML_TYPE);
->>>>>>> 7db5d37fbf6feb47aa71ce6db4f53597d1ca36db
 	}
 
 	public Model parseRdf(String ontologyUrl) throws IllegalStateException,ClientProtocolException, IOException 
