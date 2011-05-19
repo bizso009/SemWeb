4e6933ec031a32c1bc6b015f295406acdcc67a52
Kushal D'Souza <kushlyn@gmail.com>
2011-05-19 14:20:37 +0100

    artistextractor

---
+++ test/uk/ac/shef/semweb/ArtistExtractorTest.java
@@ -0,0 +1,67 @@
+package uk.ac.shef.semweb;
+
+import java.io.IOException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPathExpressionException;
+
+import org.apache.http.client.ClientProtocolException;
+import org.junit.Before;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+import com.hp.hpl.jena.rdf.model.Model;
+
+import junit.framework.TestCase;
+
+
+public class ArtistExtractorTest extends TestCase
+{
+	private ArtistExtractor artistExtractor;
+	
+	@Override
+	@Before
+	public void setUp()
+	{
+		String ontologyUrl = "http://poplar.dcs.shef.ac.uk/~u0082/intelweb2/intelweb.rdf";
+		String xmlUrl = "http://poplar.dcs.shef.ac.uk/~u0082/intelweb2/%3fq=artist/471/xml";
+		XMLExtractorImpl xmlExtractorImpl = new XMLExtractorImpl();
+		try 
+		{
+			Model model = xmlExtractorImpl.parseRdf(ontologyUrl);
+			try 
+			{
+				Document document = xmlExtractorImpl.loadXml(xmlExtractorImpl.openUrl(xmlUrl).getContent());
+				artistExtractor = new ArtistExtractor(model, document, xmlUrl);		
+			} catch (SAXException e) 
+			{
+				e.printStackTrace();
+			} catch (ParserConfigurationException e) 
+			{
+				e.printStackTrace();
+			}
+		} catch (IllegalStateException e) 
+		{
+			e.printStackTrace();
+		} catch (ClientProtocolException e) 
+		{
+			e.printStackTrace();
+		} catch (IOException e) 
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public void testGetArtistName()
+	{
+		String name;
+		try {
+			name = artistExtractor.getArtistName();
+			assertEquals("Franz Ferdinand", name);
+		} catch (XPathExpressionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+}
