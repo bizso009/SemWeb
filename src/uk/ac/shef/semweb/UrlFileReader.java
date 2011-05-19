7db5d37fbf6feb47aa71ce6db4f53597d1ca36db
Kushal D'Souza <kushlyn@gmail.com>
2011-05-19 05:33:25 +0100

    Indentations

--- src/uk/ac/shef/semweb/UrlFileReader.java
+++ src/uk/ac/shef/semweb/UrlFileReader.java
@@ -20,54 +20,57 @@ import com.hp.hpl.jena.rdf.model.ModelFactory;
 
 public class UrlFileReader {
 
-    public static final String INPUT_PATH = "input/internalLinks.txt";
-    public static final String XML_TYPE = "text/xml";
-    public static final String ONTOLOGY_URL = "http://poplar.dcs.shef.ac.uk/~u0082/intelweb2/intelweb.rdf";
-    /**
-     * Reads a file and returns each line as a String
-     */
-    public List<String> readFile(String inputPath) throws FileNotFoundException {
-	File inputFile = new File(inputPath);
-	Scanner scanner = new Scanner(inputFile);
-	
-	List<String> urls = new ArrayList<String>();
-	while (scanner.hasNext()){
-	    urls.add(scanner.nextLine());
-	}
-	return urls;
-    }
+	public static final String INPUT_PATH = "input/internalLinks.txt";
+	public static final String XML_TYPE = "text/xml";
+	public static final String ONTOLOGY_URL = "http://poplar.dcs.shef.ac.uk/~u0082/intelweb2/intelweb.rdf";
 
-    /**
-     * Opens the url
-     */
-    public HttpEntity openUrl(String url) throws ClientProtocolException, IOException {
-	HttpClient httpclient = new DefaultHttpClient();
-	HttpGet httpget = new HttpGet(url);
-	HttpResponse response = httpclient.execute(httpget);
-	HttpEntity entity = response.getEntity();
-	return entity;
-    }
+	/**
+	 * Reads a file and returns each line as a String
+	 */
+	public List<String> readFile(String inputPath) throws FileNotFoundException {
+		File inputFile = new File(inputPath);
+		Scanner scanner = new Scanner(inputFile);
 
-    /**
-     * Is the entity an xml file?
-     */
-    public boolean isXML(HttpEntity entity) {
-	return entity.getContentType().getValue().equals(XML_TYPE);
-    }
-
-    public Model parseRdf(String ontologyUrl) throws IllegalStateException, ClientProtocolException, IOException {
-	 Model model = ModelFactory.createDefaultModel();
-	 
-	 // use the FileManager to find the input file
-	 InputStream in = openUrl(ontologyUrl).getContent();
-	if (in == null) {
-	    throw new IllegalArgumentException(
-	                                 "File: " + ontologyUrl + " not found");
+		List<String> urls = new ArrayList<String>();
+		while (scanner.hasNext()) {
+			urls.add(scanner.nextLine());
+		}
+		return urls;
 	}
 
-	// read the RDF/XML file
-	model.read(in, null);
+	/**
+	 * Opens the url
+	 */
+	public HttpEntity openUrl(String url) throws ClientProtocolException,IOException 
+	{
+		HttpClient httpclient = new DefaultHttpClient();
+		HttpGet httpget = new HttpGet(url);
+		HttpResponse response = httpclient.execute(httpget);
+		HttpEntity entity = response.getEntity();
+		return entity;
+	}
 
-	return model;
-    }
+	/**
+	 * Is the entity an xml file?
+	 */
+	public boolean isXML(HttpEntity entity) {
+		return entity.getContentType().getValue().equals(XML_TYPE);
+	}
+
+	public Model parseRdf(String ontologyUrl) throws IllegalStateException,ClientProtocolException, IOException 
+	{
+		Model model = ModelFactory.createDefaultModel();
+
+		// use the FileManager to find the input file
+		InputStream in = openUrl(ontologyUrl).getContent();
+		if (in == null) 
+		{
+			throw new IllegalArgumentException("File: " + ontologyUrl + " not found");
+		}
+
+		// read the RDF/XML file
+		model.read(in, null);
+
+		return model;
+	}
 }
