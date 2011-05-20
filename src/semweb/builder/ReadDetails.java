91719d4b4755a19114a6d52f827b05edb174bff2
Kushal D'Souza <kushlyn@gmail.com>
2011-05-20 23:57:02 +0100

    Yes

--- src/semweb/builder/ReadDetails.java
+++ src/semweb/builder/ReadDetails.java
@@ -1,6 +1,16 @@
 package semweb.builder;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
+
+import org.apache.http.client.ClientProtocolException;
+
+import uk.ac.shef.semweb.RdfBuilder;
+
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.util.FileManager;
 
 
 /**
@@ -10,6 +20,24 @@ import java.util.ArrayList;
  */
 public abstract class ReadDetails 
 {
+	
+	protected Model ontology;
+	private String ontologyFile = "output/XMLFileTriples.rdf";
+	
+	/**
+	 * Class constructor initiates reading of details
+	 */
+	public ReadDetails()
+	{
+		try {
+			populateOntology();
+		} catch (Exception exception)
+		{
+			System.out.println("Error");
+			// Handle code in event of the exception
+		}
+	}
+	
 	/**
 	 * Method that reads all the details of each item.
 	 * 
@@ -22,4 +50,35 @@ public abstract class ReadDetails
 	 * @return
 	 */
 	public abstract ArrayList getItems();
+	
+	/**
+	 * Reads an rdf triples file into memory
+	 * @throws IOException 
+	 * @throws ClientProtocolException 
+	 * @throws IllegalStateException 
+	 */
+	public void populateOntology() throws IllegalStateException, ClientProtocolException, IOException
+	{
+		Model model = ModelFactory.createDefaultModel();
+
+        // use the FileManager to find the input file
+		 InputStream in = FileManager.get().open( ontologyFile );
+        if (in == null)
+        {
+        	System.out.println("Invalid File Name");
+        }
+        // read the RDF/XML file
+        model.read(in, null);
+        model.setNsPrefix("com4280", RdfBuilder.RDF_BASE);
+        
+        this.ontology = model;
+	}
+
+	public void setOntology(Model ontology) {
+		this.ontology = ontology;
+	}
+
+	public Model getOntology() {
+		return ontology;
+	}
 }
