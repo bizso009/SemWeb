20d5242309471755901775a94e4f111daf1423ad
Zsolt Bitvai <bitvaizs@gmail.com>
2011-05-22 05:58:36 +0100

    fix

--- src/uk/ac/shef/semweb/RdfBuilder.java
+++ src/uk/ac/shef/semweb/RdfBuilder.java
@@ -155,6 +155,7 @@ public abstract class RdfBuilder
         this.withWebServices = withWebServices;
 
         this.properties = new IntelWebProperties(ontology);
+        this.nodes = new IntelWebNodes();
 
   
 
